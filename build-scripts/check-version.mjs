e9827971ccea25f8d95f50fc2e0aebf055ceb491
DJPiplup <djpiplup.dev@gmail.com>
2024-12-20 12:44:19 -0800

    Store local version

--- build-scripts/check-version.mjs
+++ build-scripts/check-version.mjs
@@ -1,7 +1,9 @@
-import { existsSync } from "fs";
+import { existsSync, writeFileSync } from "fs";
 import pkgjson from '../package.json' with {type:'json'};
 
 if(existsSync(`./site/${pkgjson.version}/app.asar`)){
     console.error(`Version ${pkgjson.version} already exists`);
     process.exit(1);
 }
+
+writeFileSync('./src/version.txt',pkgjson.version);
